22ab706a5dceb8943ab7503d03a16464f868f308
izzanka <izzancoc.46@gmail.com>
2021-07-06 11:35:05 +0700

    remove ability to register

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
