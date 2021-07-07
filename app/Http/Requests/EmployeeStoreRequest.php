bebbd54e94e625c0b2658353d2331a2a142d4151
izzanka <izzancoc.46@gmail.com>
2021-07-07 16:30:22 +0700

    create request validation for employees & companies

---
+++ app/Http/Requests/EmployeeStoreRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EmployeeStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'companie_id' => 'required|numeric',
+            'email' => 'required|email|string',
+            'phone' => 'required|numeric|min:15'
+        ];
+    }
+}
