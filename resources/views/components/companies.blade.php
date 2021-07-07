bc0562e53066f23e76067547cf620d5c546ee6ef
izzanka <izzancoc.46@gmail.com>
2021-07-07 16:28:19 +0700

    create component to displaying total data in table

---
+++ resources/views/components/companies.blade.php
@@ -0,0 +1,7 @@
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('companies.index') }}">
+        Companies
+        <span class="badge badge-primary badge-pill">
+            {{ $companies ?? 0 }}</span>
+    </a>
+</li>
---
+++ resources/views/components/employees.blade.php
@@ -0,0 +1,7 @@
+<li class="nav-item">
+    <a class="nav-link" href="{{ route('employees.index') }}">
+        Employees
+        <span class="badge badge-success badge-pill">
+            {{ $employees ?? 0 }}</span>
+    </a>
+</li>
