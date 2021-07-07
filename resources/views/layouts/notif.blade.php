f6a075abd825d09afb93ed46586d3ea24e8b4874
izzanka <izzancoc.46@gmail.com>
2021-07-07 16:26:52 +0700

    create notif & error blade

---
+++ resources/views/layouts/notif.blade.php
@@ -0,0 +1,10 @@
+@if (session()->has('message'))
+<div class="col-12">
+    <div class="alert alert-{{ session('message')['class'] }} alert-dismissible fade show" role="alert">
+        <strong>{{ session('message')['text'] }}</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+</div>
+@endif
