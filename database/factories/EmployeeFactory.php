ca6e9a00dda8e3ae946265cf16714d2161277ded
izzanka <izzancoc.46@gmail.com>
2021-07-10 09:22:35 +0700

    add factory & seeder employee

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EmployeeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Employee::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail(),
+            'companie_id' => 7,
+            'phone' => \rand(111111111111,999999999999)
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(1)->create();
+        // \App\Models\User::factory(1)->create();
+        \App\Models\Employee::factory(100)->create();
     }
 }
