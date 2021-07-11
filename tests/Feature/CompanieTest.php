0c82b252770a4d1a89f0f34a1fe3c38cb6b09af2
izzanka <izzancoc.46@gmail.com>
2021-07-11 19:57:56 +0700

    add testing

---
+++ tests/Feature/CompanieTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Companie;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CompanieTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public $user;
+
+    protected function setUp() : void{
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+    
+    //    public function testDenyHomeWhenNotLogginIn(){
+    //        $response = $this->get('/');
+    //        $response->assertRedirect(route('login'));
+    //    }
+
+   public function testAllowCompanieIndex(){
+       $response = $this->get(route('companies.index'));
+       $response->assertOk();
+   }
+
+   public function testAllowCompanieCreate(){
+        $response = $this->get(route('companies.create'));
+        $response->assertOk();
+   }
+
+//    public function testAllowCompanieStore()
+//    {
+//        $params = [
+//         'name' => 'xiomi',
+//         'email' => 'xiomi@gmail.com',
+//         'logo' => 'xiomi.png',
+//         'website' => 'xiomi.com',
+//        ];
+
+//        $response = $this->post(route('companies.store',$params));
+//        $response->assertRedirect(route('companies.create'));
+
+//        $this->assertDatabaseHas('companies',$params);
+//    }
+
+   public function testAllowCompanieEdit(){
+        $companie = Companie::factory()->create();
+
+        $response = $this->get(route('companies.edit',$companie->id));
+        $response->assertOk();
+   }
+
+//    public function testAllowCompanieUpdate()
+//    {
+//        $companie = Companie::factory()->create();
+
+//        $params = [
+//         'name' => 'xiomi',
+//         'email' => 'xiomi@gmail.com',
+//         'logo' => 'xiomi.png',
+//         'website' => 'xiomi.com',
+//        ];
+
+//        $response = $this->put(route('companies.update',$companie->id),$params);
+//        $response->assertRedirect(route('companies.edit',$companie->id));
+
+//        $params['name'] = 'Updated';
+    
+//        $this->assertDatabaseHas('companies',$params);
+//    }
+
+   public function testAllowCompanieDelete()
+   {
+        $companie = Companie::factory()->create();
+
+        $response = $this->delete(route('companies.destroy',$companie->id));
+        $response->assertRedirect(route('companies.index'));
+
+        $this->assertDatabaseMissing('companies',['id' => $companie->id]);
+   }
+
+}
---
+++ tests/Feature/EmployeeTest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Employee;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EmployeeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public $user;
+
+    protected function setUp() : void{
+        parent::setUp();
+        $this->user = User::factory()->create();
+        $this->actingAs($this->user);
+    }
+
+//    public function testDenyHomeWhenNotLogginIn(){
+//        $response = $this->get('/');
+//        $response->assertRedirect(route('login'));
+//    }
+
+   public function testAllowEmployeeIndex(){
+       $response = $this->get(route('employees.index'));
+       $response->assertOk();
+       $response->assertViewIs('employee.index');
+   }
+
+   public function testAllowEmployeeCreate(){
+        $response = $this->get(route('employees.create'));
+        $response->assertOk();
+        $response->assertViewIs('employee.create');
+   }
+
+   public function testAllowEmployeeStore()
+   {
+       $params = [
+        'first_name' => 'izzan',
+        'last_name' => 'ka',
+        'email' => 'izzan@gmail.com',
+        'companie_id' => 7,
+        'phone' => 949494949494
+       ];
+
+       $response = $this->post(route('employees.store',$params));
+       $response->assertRedirect(route('employees.create'));
+
+       $this->assertDatabaseHas('employees',$params);
+   }
+
+   public function testAllowEmployeeEdit(){
+        $employee = Employee::factory()->create();
+
+        $response = $this->get(route('employees.edit',$employee->id));
+        $response->assertOk();
+        $response->assertViewIs('employee.edit');
+   }
+
+   public function testAllowEmployeeUpdate()
+   {
+       $employee = Employee::factory()->create();
+
+       $params = [
+        'first_name' => 'Updated',
+        'last_name' => 'ka',
+        'email' => 'izzan@gmail.com',
+        'companie_id' => 7,
+        'phone' => 949494949494
+       ];
+
+       $response = $this->put(route('employees.update',$employee->id),$params);
+       $response->assertRedirect(route('employees.edit',$employee->id));
+
+       $params['first_name'] = 'Updated';
+    
+       $this->assertDatabaseHas('employees',$params);
+   }
+
+   public function testAllowEmployeeDelete()
+   {
+        $employee = Employee::factory()->create();
+
+        $response = $this->delete(route('employees.destroy',$employee->id));
+        $response->assertRedirect(route('employees.index'));
+
+        $this->assertDatabaseMissing('employees',['id' => $employee->id]);
+   }
+
+   
+
+}
