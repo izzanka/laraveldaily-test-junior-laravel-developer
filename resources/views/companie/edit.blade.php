812b6afbbe274b70e7fd2489e69d3f25041a24a4
izzanka <izzancoc.46@gmail.com>
2021-07-17 13:01:12 +0700

    edit companie view

--- resources/views/companie/edit.blade.php
+++ resources/views/companie/edit.blade.php
@@ -15,24 +15,24 @@
                     <a href="{{ route('companies.index') }}" class="btn btn-sm btn-secondary float-right"> {{ __('companie.btn4') }}</a>
                 </div>
                 <div class="card-body">
-                    <form action="{{ route('companies.update',$companie->id) }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('companies.update',$company->id) }}" method="POST" enctype="multipart/form-data">
                         @csrf
                         @method('PUT')
                         <div class="row">
                           <div class="col-6">
                             <label for=""> {{ __('companie.table1') }}</label>
-                            <input type="text" class="form-control" name="name" value="{{ $companie->name }}">
+                            <input type="text" class="form-control" name="name" value="{{ $company->name }}">
                             @include('layouts.error', ['name' => 'name'])
                           </div>
                           <div class="col-6">
                             <label for=""> {{ __('companie.table2') }}</label>
-                            <input type="email" class="form-control" name="email" value="{{ $companie->email }}">
+                            <input type="email" class="form-control" name="email" value="{{ $company->email }}">
                             @include('layouts.error', ['name' => 'email'])
                         </div>
                       </div>
                       <div class="row mt-3">
                             <div class="col-6">
-                                <img id="output" src="{{ Storage::url('logos/' . $companie->logo) }}" width="100" height="100" alt="Logo Preview" class="img-thumbnail">
+                                <img id="output" src="{{ Storage::url('logos/' . $company->logo) }}" width="100" height="100" alt="Logo Preview" class="img-thumbnail">
                                 <div class="custom-file mt-3">
                                     <input type="file" name="logo" class="custom-file-input" accept="image/*" onchange="document.getElementById('output').src = window.URL.createObjectURL(this.files[0])">
                                     @include('layouts.error', ['name' => 'logo'])
@@ -42,7 +42,7 @@
                             <div class="col-6 ">
                                 <label for=""> {{ __('companie.table4') }}</label>
                                 <div class="mt-2">
-                                    <input type="text" name="website" class="form-control" value="{{ $companie->website }}">
+                                    <input type="text" name="website" class="form-control" value="{{ $company->website }}">
                                     @include('layouts.error', ['name' => 'website'])
                                 </div>
                             </div>
