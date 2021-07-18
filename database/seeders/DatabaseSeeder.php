570efd617aa52d85c8dc6b0690a6e656e5179be1
izzanka <izzancoc.46@gmail.com>
2021-07-18 12:46:20 +0700

    comment seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(1)->create();
-        \App\Models\Employee::factory(1000)->create();
-        \App\Models\Companie::factory(1000)->create();
+        // \App\Models\Employee::factory(1000)->create();
+        // \App\Models\Companie::factory(1000)->create();
     }
 }
